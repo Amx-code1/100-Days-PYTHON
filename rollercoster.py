1da0e242d51a69e3a636bf725ab8f596128d61f2
Amx-code1 <xtreme.amx.45@gmail.com>
2025-05-11 11:03:13 +0530

    Add files via upload

---
+++ rollercoster.py
@@ -0,0 +1,9 @@
+print("Welcome to rollercoster!")
+height = int(input('what is your height in cm ?'))
+
+if height > 120:
+    print("you can ride the rollercoster!")
+else:
+    print("sorry you have to grow taller before you can ride")
+    
+    
---
+++ rollercoster3.py
@@ -0,0 +1,25 @@
+print("Welcome to rollercoster!")
+height = int(input('what is your height in cm ?'))
+bill = 0
+
+if height >= 120:
+    print("you can ride the rollercoster!")
+    age = int(input("what is your age ? "))
+    if age < 12:
+        bill = 5
+        print("please pay $5.")
+    elif age <=18:
+        bill = 7
+        print("please pay $7.")
+    else:
+        bill = 12
+        print("please pay $12.")
+    wants_photo = input("Do you want a photo taken? Y or N.")
+    if wants_photo == "Y":
+        bill += 3
+        
+    print(f"Your final bill is {bill }")
+else:
+    print("sorry you have to grow taller before you can ride")
+    
+    
